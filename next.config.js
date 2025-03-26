f786e611fb1b3c23db0a946b9d1a6ad8c44b31d2
Vinícius Dias <diasvinicius0401@gmail.com>
2025-03-26 19:10:28 -0300

    Get countries data from rest countries API

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'placehold.co',
-        },
-      ],
-      dangerouslyAllowSVG: true, // Permite imagens SVG externas
-    },
-  }
-  
-  module.exports = nextConfig
-  
-  
+const nextConfig = {  // Aqui estava o erro antes
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+        pathname: "/wikipedia/commons/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+};
+
+module.exports = nextConfig;
